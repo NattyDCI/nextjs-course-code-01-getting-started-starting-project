d28d19a4c8404b224f9bb19bc34cfba735ee3ad5
NathaliaPadron <nbavutti@gmail.com>
2024-10-31 11:24:49 +0100

    finished

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,27 @@
+import { useRouter } from "next/router"
 
 function ClientProjectsPage() {
+
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    //load data...
+    // we can write it like this.. 
+    // router.push("/clients/max/projecta")
+    // ...or
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { 
+        id: "max",
+        clientprojectid: "projecta"}
+    })
+
+  }
   return (
-    <div>the Projects of a given client</div>
+    <div>
+      <h1>the Projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A:</button>
+    </div>
   )
 }
 
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,20 +1,31 @@
 import Link from "next/link";
 
 function ClientsPage() {
-
-    const clients = [
-        { id: "max", name: "maximilian" },
-        { id: "nat", name: "nathalia" },
-    ];
+  const clients = [
+    { id: "max", name: "maximilian" },
+    { id: "nat", name: "nathalia" },
+  ];
+  // you can write it like this... or you can write it like the code bellow in the return statement.
+  // <Link href={`clients/${client.name}`}>{client.name}</Link>
 
   return (
     <>
       <div>ClientsPage</div>
       <ul>
         {clients.map((client) => (
-            <li key={client.id}>
-                <Link href={`clients/${client.name}`}>{client.name}</Link>
-            </li>
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: {
+                  // id, because thats our dinamic segment identifier
+                  id: client.id,
+                },
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
         ))}
       </ul>
     </>
